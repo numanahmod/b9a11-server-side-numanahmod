b8a8e0d8268c19bafeb099912d1953ee93239348
Your Name <you@example.com>
2024-05-11 23:04:24 +0900

    api making is ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -37,13 +37,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const jobsCollection = client.db('postJobsDB').collection('postedJobs')
+    // get all jobs data from db 
+    app.get('/postedJobs', async(req, res) =>{
+        const result = await jobsCollection.find().toArray()
+
+        res.send(result)
+    })
+    //get a single job dtls
+
+    app.get('/job/:id', async(req, res) =>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id) }
+        const result = await jobsCollection.findOne(query)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
